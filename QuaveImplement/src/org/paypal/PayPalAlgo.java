67d49b1364db8138091b8200b729bff7e06a1dc4
Samelia <Samelia@DESKTOP-K0RLPAB>
2018-05-24 19:29:28 +0530

    Queve implementation

---
+++ QuaveImplement/src/org/paypal/PayPalAlgo.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.paypal;
+
+/**
+ * @author Samelia
+ *
+ */
+public class PayPalAlgo {
+
+	public static void main(String[] args) {
+		System.out.println("Anil Kumar");
+	}
+}
